03ac4a503a74685ec3392bc2b551df5290943051
hayamiya <0510ttk@gmail.com>
2025-06-06 15:35:25 +0900

    initial commit

---
+++ Invoke-TopologicalSort.ps1
@@ -0,0 +1,72 @@
+﻿function Invoke-TopologicalSort {
+    param (
+        [Parameter(Mandatory)]
+        [hashtable]$Graph,
+        [switch]$Reverse
+    )
+
+    # 各ノードの入力次数を保存するハッシュテーブルを初期化
+    $inDegree = @{}
+    foreach ($node in $Graph.Keys) {
+        $inDegree[$node] = 0
+    }
+
+    # 各ノードの入力次数を計算
+    foreach ($node in $Graph.Keys) {
+        foreach ($neighbor in $Graph[$node]) {
+            $inDegree[$neighbor]++
+        }
+    }
+
+    # 入力次数が0のノードをキューに追加
+    $queue = [System.Collections.Queue]::new()
+    foreach ($node in $inDegree.Keys) {
+        if ($inDegree[$node] -eq 0) {
+            $queue.Enqueue($node)
+        }
+    }
+
+    $sortedList = @()
+
+    # キューが空になるまで続ける
+    while ($queue.Count -gt 0) {
+        $node = $queue.Dequeue()
+        $sortedList += $node
+
+        # 隣接ノードの入力次数を減らし、入力次数が0になったらキューに追加
+        foreach ($neighbor in $Graph[$node]) {
+            $inDegree[$neighbor]--
+            if ($inDegree[$neighbor] -eq 0) {
+                $queue.Enqueue($neighbor)
+            }
+        }
+    }
+
+    # ソートされたリストの長さがグラフのノード数と一致しない場合、グラフにはサイクルが存在する
+    if ($sortedList.Count -ne $Graph.Count) {
+        Throw("The graph has a cycle involving the node and cannot be topologically sorted.")
+    }
+
+    if ($Reverse) {
+        [array]::Reverse($sortedList)
+    }
+    return $sortedList
+}
+
+# Example
+$Graph = @{
+    'A' = @('E')
+    'B' = @('A')
+    'C' = @()
+    'D' = @('B', 'C')
+    'E' = @()
+    'F' = @()
+}
+
+try {
+    $sortedOrder = Invoke-TopologicalSort -graph $Graph
+    Write-Host "Topologically sorted order: $sortedOrder"
+}
+catch {
+    Write-Error $_.Exception.Message
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Invoke-TopologicalSort
+
+## Overview
+
+`Invoke-TopologicalSort` is a PowerShell function that performs a topological sort on a directed acyclic graph (DAG) represented as a hashtable of adjacency lists. It returns an ordered list of nodes such that for every directed edge `U -> V`, node `U` comes before `V` in the output. Optionally, you can reverse the sorted order.
+
+This can be useful for dependency resolution, build pipelines, task ordering, or any scenario where you need to process nodes in a sequence that respects their dependencies.
